8698c22ea23c40dbeddf61bc032996c69076d587
Ayrton Briceno <iqbriceno@gmail.com>
2024-06-13 19:15:57 -0400

    Initial Commit

---
+++ WeatherForecast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherForecast
+//
+//  Created by Ayrton Briceno on 6/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
