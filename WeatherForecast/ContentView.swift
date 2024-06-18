33a9915903f694d7f6dca2403fcc2824afccf1f7
Ayrton Briceno <iqbriceno@gmail.com>
2024-06-17 22:57:48 -0400

    Added conditionals for styling

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            DayForecast(day: "Mon", high: 70, low: 50)
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
             
-            DayForecast(day: "Tue", high: 60, low: 40)
+            DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
         }
     }
 }
@@ -23,16 +23,45 @@ struct ContentView: View {
 
 struct DayForecast: View {
     let day: String
+    let isRainy: Bool
     let high: Int
     let low: Int
     
+    
+    //  Rainy condition
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        }
+        else {
+            return "sun.max.fill"
+        }
+    }
+
+    //  Icon color
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        }
+        else {
+            return Color.yellow
+        }
+    }
+
+    //  Canvas
     var body: some View {
         VStack {
             Text(day)
-            Image(systemName: "sun.max.fill")
-                .foregroundStyle(Color.yellow)
+                .font(Font.headline)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(Font.largeTitle)
+                .padding(5)
             Text("High: \(high)")
+                .fontWeight(Font.Weight.semibold)
             Text("Low: \(low)")
+                .fontWeight(Font.Weight.medium)
+                .foregroundStyle(Color.secondary)
         }
         .padding()
     }
